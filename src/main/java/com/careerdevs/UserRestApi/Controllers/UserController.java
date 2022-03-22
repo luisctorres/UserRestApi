005e09ec57c7daa21265140c7ac893eb7ca54851
luisctorres <Scratch2c@gmail.com>
2022-03-22 17:44:23 -0400

    Initial commit

---
+++ src/main/java/com/careerdevs/UserRestApi/Controllers/UserController.java
@@ -0,0 +1,4 @@
+package com.careerdevs.UserRestApi.Controllers;
+
+public class UserController {
+}
---
+++ src/test/java/com/careerdevs/UserRestApi/UserRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.careerdevs.UserRestApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
