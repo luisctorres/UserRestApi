b4090e14a09abad6a2d9bcb8a3aca61f0e3c5420
luisctorres <Scratch2c@gmail.com>
2022-03-25 23:40:17 -0400

    need to implement auth with bearer token

--- src/main/java/com/careerdevs/UserRestApi/Controllers/UserController.java
+++ src/main/java/com/careerdevs/UserRestApi/Controllers/UserController.java
@@ -1,4 +1,23 @@
 package com.careerdevs.UserRestApi.Controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@RequestMapping("/api/user")
 public class UserController {
+
+
+@GetMapping("/{id}")
+    public Object getOneUser(@PathVariable("id") String userId,
+                             RestTemplate restTemplate){
+
+    String url = "https://gorest.co.in/public/v2/users?access-token=640b92322772b544c91b13e11b180ac3e24157879f6da64af338e07ba655fd17";
+    return restTemplate.getForObject(url, Object.class);
+    }
+
+
 }
