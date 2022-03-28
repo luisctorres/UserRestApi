90e353d493bebeeff0919b1e50ee4eeecbbf0d8c
luisctorres <Scratch2c@gmail.com>
2022-03-28 19:37:00 -0400

    Environment variable not working as expected.

--- src/main/java/com/careerdevs/UserRestApi/Controllers/UserController.java
+++ src/main/java/com/careerdevs/UserRestApi/Controllers/UserController.java
@@ -1,5 +1,12 @@
 package com.careerdevs.UserRestApi.Controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,16 +15,41 @@ import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("/api/user")
-public class UserController {
+    public class UserController {
+
+        @Autowired
+        Environment env;
+
+        @GetMapping("/token")
+        public String getToken() {
+            return env.getProperty("GO_REST_TOKEN");
+        }
 
 
-@GetMapping("/{id}")
+
+
+    @GetMapping("/{id}")
     public Object getOneUser(@PathVariable("id") String userId,
-                             RestTemplate restTemplate){
+                             RestTemplate restTemplate) {
 
-    String url = "https://gorest.co.in/public/v2/users";
-    return restTemplate.getForObject(url, Object.class);
+        try {
+            String url = "https://gorest.co.in/public/v2/users" + userId;
+            HttpHeaders headers = new HttpHeaders();
+            //headers.setBearerAuth(env.getProperty("GO_REST_TOKEN"));
+
+            HttpEntity request = new HttpEntity(headers);
+
+            return restTemplate.exchange(
+                    url,
+                    HttpMethod.GET,
+                    request,
+                    Object.class
+            );
+        } catch (Exception exception) {
+            return "404: No user exists with the ID " + userId;
+        }
     }
-
-
 }
+
+
+
