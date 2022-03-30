917dca3d977df230eb8e84f98cbffa6b05c8f269
luisctorres <Scratch2c@gmail.com>
2022-03-29 20:49:23 -0400

    added TODO.

--- src/main/java/com/careerdevs/UserRestApi/Controllers/UserController.java
+++ src/main/java/com/careerdevs/UserRestApi/Controllers/UserController.java
@@ -1,12 +1,13 @@
 package com.careerdevs.UserRestApi.Controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.core.env.Environment;
+
+
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,27 +18,28 @@ import org.springframework.web.client.RestTemplate;
 @RequestMapping("/api/user")
     public class UserController {
 
-        @Autowired
-        Environment env;
+
+    //TODO Fix this error with the Environment Variable
+
+    @Autowired
+    Environment env;
 
         @GetMapping("/token")
         public String getToken() {
-            return env.getProperty("GO_REST_TOKEN");
+            return this.env.getProperty("GO_REST_TOKEN");
         }
 
 
-
-
-    @GetMapping("/{id}")
-    public Object getOneUser(@PathVariable("id") String userId,
+        @GetMapping("/{id}")
+        public Object getOneUser(@PathVariable("id") String userId,
                              RestTemplate restTemplate) {
 
         try {
             String url = "https://gorest.co.in/public/v2/users" + userId;
             HttpHeaders headers = new HttpHeaders();
-            //headers.setBearerAuth(env.getProperty("GO_REST_TOKEN"));
+            headers.setBearerAuth(env.getProperty("GO_REST_TOKEN"));
 
-            HttpEntity request = new HttpEntity(headers);
+            HttpEntity<Object> request = new HttpEntity<>(headers);
 
             return restTemplate.exchange(
                     url,
