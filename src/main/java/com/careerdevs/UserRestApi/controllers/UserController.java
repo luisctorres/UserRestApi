c9d8a36405e4b9a9249fc7b05431d1db166f9d4f
luisctorres <Scratch2c@gmail.com>
2022-03-31 19:28:03 -0400

    added UserModel and Post mapping

---
+++ src/main/java/com/careerdevs/UserRestApi/controllers/UserController.java
@@ -0,0 +1,71 @@
+package com.careerdevs.UserRestApi.controllers;
+
+import com.careerdevs.UserRestApi.models.UserModel;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.core.env.Environment;
+
+
+import org.springframework.http.*;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@RequestMapping("/api/user")
+    public class UserController {
+
+
+    @Autowired
+    Environment env;
+
+    private RestTemplate restTemplate;
+
+    @GetMapping("/token")
+        public String getToken() {
+            return this.env.getProperty("GO_REST_TOKEN");
+        }
+
+    //Get Request - Returns 1 user
+    @GetMapping("/{id}")
+    public Object getOneUser(@PathVariable("id") String userId,
+                             RestTemplate restTemplate) {
+        try {
+        String url = "https://gorest.co.in/public/v2/users/" + userId;
+        HttpHeaders headers = new HttpHeaders();
+        headers.setBasicAuth("640b92322772b544c91b13e11b180ac3e24157879f6da64af338e07ba655fd17");
+
+        HttpEntity<Object> request = new HttpEntity<>(headers);
+
+        return restTemplate.exchange(
+                                    url,
+                                    HttpMethod.GET,
+                                    request,
+                                    Object.class
+                                    );
+            }
+        catch (Exception exception) {
+                return "404: No user exists with the ID " + userId;
+            }
+        }
+
+    //Post mapping to add user
+    @PostMapping("/")
+    public ResponseEntity postUser(
+            RestTemplate restTemplate,
+            @RequestBody UserModel newUser) {
+
+            String url = "https://gorest.co.in/public/v2/users/";
+            HttpHeaders headers = new HttpHeaders();
+            headers.setBasicAuth("640b92322772b544c91b13e11b180ac3e24157879f6da64af338e07ba655fd17");
+
+            HttpEntity<UserModel> request = new HttpEntity<UserModel>(newUser);
+            return restTemplate.postForEntity(url, request, UserModel.class);
+
+    }
+
+
+
+}
+
+
+
---
+++ src/main/java/com/careerdevs/UserRestApi/models/UserModel.java
@@ -0,0 +1,48 @@
+package com.careerdevs.UserRestApi.models;
+
+public class UserModel {
+
+    private int id;
+    private String name;
+    private String email;
+    private String gender;
+    private String status;
+
+    public UserModel() {
+    }
+
+    public UserModel(String name, String email, String gender, String status) {
+        this.name = name;
+        this.email = email;
+        this.gender = gender;
+        this.status = status;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public String generateReport() {
+        return name + " is currently " + status + ". You can reach them at: " + email;
+    }
+
+
+
+
+}
