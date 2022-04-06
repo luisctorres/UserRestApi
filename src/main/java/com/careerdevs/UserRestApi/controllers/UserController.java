9bb19f433bed332bd8ad2078bceef720bf949496
luisctorres <Scratch2c@gmail.com>
2022-04-05 20:57:02 -0400

    april 5th

--- src/main/java/com/careerdevs/UserRestApi/controllers/UserController.java
+++ src/main/java/com/careerdevs/UserRestApi/controllers/UserController.java
@@ -30,21 +30,21 @@ import org.springframework.web.client.RestTemplate;
     public Object getOneUser(@PathVariable("id") String userId,
                              RestTemplate restTemplate) {
         try {
-        String url = "https://gorest.co.in/public/v2/users/" + userId;
-        HttpHeaders headers = new HttpHeaders();
-        headers.setBasicAuth(getToken());
+            String url = "https://gorest.co.in/public/v2/users?access-token=";
 
-        HttpEntity<Object> request = new HttpEntity<>(headers);
+            HttpHeaders headers = new HttpHeaders();
+            headers.setBearerAuth(getToken());
+            HttpEntity<UserModel> request = new HttpEntity<>(headers);
 
-        return restTemplate.exchange(
+             return restTemplate.exchange(
                                     url,
                                     HttpMethod.GET,
                                     request,
-                                    Object.class
+                                    UserModel.class
                                     );
             }
-        catch (Exception exception) {
-                return "404: No user exists with the ID " + userId;
+             catch (Exception exception) {
+                    return "404: No user exists with the ID " + userId;
             }
         }
 
@@ -54,14 +54,24 @@ import org.springframework.web.client.RestTemplate;
             RestTemplate restTemplate,
             @RequestBody UserModel newUser) {
 
-        String url = "https://gorest.co.in/public/v2/users?access-token=640b92322772b544c91b13e11b180ac3e24157879f6da64af338e07ba655fd17";
-        HttpHeaders headers = new HttpHeaders();
-        //headers.setBearerAuth(getToken());
-//        UserModel newUser = new UserModel(name, email, gender, status);
+        try {
+            String url = "https://gorest.co.in/public/v2/users?access-token=640b92322772b544c91b13e11b180ac3e24157879f6da64af338e07ba655fd17";
+            HttpHeaders headers = new HttpHeaders();
+            //headers.setBearerAuth(getToken());
+            HttpEntity<UserModel> request = new HttpEntity<>(newUser);
+            return restTemplate.postForEntity(url, request, UserModel.class);
+        }
 
-        HttpEntity<UserModel> request = new HttpEntity<>(newUser);
-        return restTemplate.postForEntity(url, request, UserModel.class);
 
+        catch (HttpClientErrorException.UnprocessableEntity e) {
+
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+
+        } catch (Exception e) {
+            System.out.println(e.getClass() + "\n" + e.getMessage());
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+
+            }
     }
 
     //put request for user
@@ -72,9 +82,7 @@ import org.springframework.web.client.RestTemplate;
     ) {
         try {
 
-            String url = "https://gorest.co.in/public/v2/users/" + updateData.getId();
-            String token = env.getProperty("GO_REST_TOKEN");
-            url += "?access-token=" + token;
+            String url = "https://gorest.co.in/public/v2/users?access-token=640b92322772b544c91b13e11b180ac3e24157879f6da64af338e07ba655fd17";
 
             HttpEntity<UserModel> request = new HttpEntity<>(updateData);
 
